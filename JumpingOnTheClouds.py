7ae7c36f4039230b1558c5e52c45cee57902e0c7
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2021-09-04 23:09:12 +0530

    Create JumpingOnTheClouds.py

---
+++ JumpingOnTheClouds.py
@@ -0,0 +1,12 @@
+    j = 0
+    for i in range(len(c)):
+        k = 0
+        try:
+            if c[k+2] == 1 or c[k+1] == 0:
+                c = c[k+1::]
+            elif c[k+2] == 0:
+                c = c[k+2::]
+            j += 1
+        except:
+            pass
+    return j
