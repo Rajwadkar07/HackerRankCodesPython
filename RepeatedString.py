7181982245b82121fb6636920d23f46ab496bef6
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2021-09-05 08:59:49 +0530

    Create RepeatedString.py

---
+++ RepeatedString.py
@@ -0,0 +1,12 @@
+    l = 0
+    count = 0
+    if len(s) == 1:
+        return n
+        exit
+    while l<n:
+        s += s
+        l = len(s)
+    for i in range(n):
+        if "a" == s[i]:
+            count += 1
+    return count
