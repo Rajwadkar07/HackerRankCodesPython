5e4613874a74b4f537d26fb439ed29a064a8d0dd
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2021-10-13 12:58:47 +0530

    Update repeatedstring.py

--- repeatedstring.py
+++ repeatedstring.py
@@ -1,13 +1,34 @@
-def repeatedString(s, n):    
-    l = 0
+import os
+
+def repeatedString(s, n):
     count = 0
-    if len(s) == 1:
-        return n
-        exit
-    while l<n:
-        s += s
-        l = len(s)
-    for i in range(n):
-        if "a" == s[i]:
+    #calculate number of a in input string
+    for i in s:
+        if i == "a":
             count += 1
-    return count
+    l = len(s)
+    #multiplier to get desired length of string
+    multiplier = n // l
+    #remainder of partial string to be added
+    remainder = n % l
+    #number of a when reminder is 0
+    final = count * multiplier
+    #if remainder is more than 0 and the remaining substring contains a, add 1 to final
+    if remainder!=0:
+        for i in range(remainder):
+            if s[i] == "a":
+                final += 1
+    return final
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
