8ef7a58ac84bb96b14f70c1ca2c8533d27647912
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2021-10-13 12:32:31 +0530

    Update jumpingontheclouds.py

--- jumpingontheclouds.py
+++ jumpingontheclouds.py
@@ -1,13 +1,32 @@
+import os
+
 def jumpingOnClouds(c):
-    j = 0
-    for i in range(len(c)):
-        k = 0
-        try:
-            if c[k+2] == 1 or c[k+1] == 0:
-                c = c[k+1::]
-            elif c[k+2] == 0:
-                c = c[k+2::]
-            j += 1
+    steps = 0
+    i = 0
+    while i<len(c):
+        #throws list index error for the last iteration
+        try: 
+            #first priority is step over 2 elements
+            if c[i+2] == 0:
+                steps += 1
+                i += 2
+                continue
         except:
-            pass
-    return j
+            break
+        #else increase step over 1 elements
+        steps += 1
+        i += 1
+    return steps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
