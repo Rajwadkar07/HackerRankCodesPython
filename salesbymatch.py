ca83be5d91f2f51efb4cd79e010e2f5c7a9ae48d
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2021-10-13 11:56:32 +0530

    Update salesbymatch.py

--- salesbymatch.py
+++ salesbymatch.py
@@ -1,16 +1,36 @@
+import os
+
 def sockMerchant(n, ar):
     num = []
-    cnt = 0
+    socks = 0
+    
+    #create list num with single elements
     for i in ar:
         if i not in num:
             num.append(i)
+    
+    #compare an element from num list with all the elements and increase the count one by one
     for i in num:
-        ind_cnt = 0
+        count = 0
         for j in ar:
             if i == j:
-                ind_cnt += 1
+                count += 1
             else:
-                pass
-        if ind_cnt >= 2:
-            cnt += ind_cnt//2
-    return cnt
+                continue
+            #if count is even, it is a pair
+            if count % 2 == 0:
+                socks += 1
+    return socks
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
